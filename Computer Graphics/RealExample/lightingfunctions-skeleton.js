5fb6a0ee55e0eec7164bf0aa8da343e75190ef64
Nathan Li <nal64753@bethel.edu>
2017-11-20 15:46:59 -0600

    working on graphix

--- Computer Graphics/RealExample/lightingfunctions-skeleton.js
+++ Computer Graphics/RealExample/lightingfunctions-skeleton.js
@@ -47,19 +47,6 @@ var prevMouseY = 0;
 var sphereverts; //local copy of vertex data
 var sphereBufferID; //buffer id
 
-//animation
-var rotate = false;
-
-//textures
-var spheretex;
-var earthtex;
-var earthimage;
-var uTextureSampler;
-var anisotropic_ext;
-var factor = 8;
-var vTexCoord;
-var earthRot = 0;
-
 window.onload = function init() {
 
     canvas = document.getElementById("gl-canvas");
@@ -78,11 +65,11 @@ window.onload = function init() {
     canvas.addEventListener("mouseup", mouse_up);
 
     //white background
-    gl.clearColor(0, 0, 0, 1.0);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
 
     //initialize rotation angles
-    xAngle = 90;
+    xAngle = 0;
     yAngle = 0;
 
     program = [];
@@ -120,14 +107,12 @@ window.onload = function init() {
         ambient_light.push(gl.getUniformLocation(program[i], "ambient_light"));
     }
 
-    initTextures();
+
     //get our sphere, 15 slices around the circle
-    generateSphere(50);
+    generateSphere(15);
 
-    switchShaders(PHONG); //start with the phong shader
+    switchShaders(UNLIT); //start with the unlit shader
 
-    window.setInterval(update, 16);
-    requestAnimationFrame(render);
 };
 
 function switchShaders(index){
@@ -151,20 +136,13 @@ function switchShaders(index){
     //Set up the connections between vertex attributes and buffers
     gl.bindBuffer(gl.ARRAY_BUFFER, sphereBufferID);
 
-    gl.vertexAttribPointer(vPosition[activeProgram], 4, gl.FLOAT, false, 40, 0); //stride is 32 bytes total for position, normal
+    gl.vertexAttribPointer(vPosition[activeProgram], 4, gl.FLOAT, false, 32, 0); //stride is 32 bytes total for position, normal
     gl.enableVertexAttribArray(vPosition[activeProgram]);
 
     if(activeProgram != 0) { //the normal vector isn't used in our unlit program
-        gl.vertexAttribPointer(vNormal[activeProgram], 4, gl.FLOAT, false, 40, 16);
+        gl.vertexAttribPointer(vNormal[activeProgram], 4, gl.FLOAT, false, 32, 16);
         gl.enableVertexAttribArray(vNormal[activeProgram]);
     }
-
-    vTexCoord = gl.getAttribLocation(program[activeProgram], "texCoord");
-    gl.vertexAttribPointer(vTexCoord, 2, gl.FLOAT, false, 40, 32);
-    gl.enableVertexAttribArray(vTexCoord);
-
-
-
 }
 
 //***********************************************
@@ -178,29 +156,24 @@ function generateSphere(subdiv){
 
     var step = (360.0 / subdiv)*(Math.PI / 180.0); //how much do we increase the angles by per triangle?
     sphereverts = [];
+
     for (var lat = 0; lat <= Math.PI ; lat += step){ //latitude (up down 0-180)
         for (var lon = 0; lon + step <= 2*Math.PI; lon += step){ //longitude (around world 0-360)
             //triangle 1
             sphereverts.push(vec4(Math.sin(lat)*Math.cos(lon), Math.sin(lon)*Math.sin(lat), Math.cos(lat), 1.0)); //position
             sphereverts.push(vec4(Math.sin(lat)*Math.cos(lon), Math.sin(lon)*Math.sin(lat), Math.cos(lat), 0.0)); //normal
-            sphereverts.push(vec2(lon/(2*Math.PI), lat/Math.PI));
             sphereverts.push(vec4(Math.sin(lat)*Math.cos(lon+step), Math.sin(lat)*Math.sin(lon+step), Math.cos(lat), 1.0)); //position
             sphereverts.push(vec4(Math.sin(lat)*Math.cos(lon+step), Math.sin(lat)*Math.sin(lon+step), Math.cos(lat), 0.0)); //normal
-            sphereverts.push(vec2((lon+step)/(2*Math.PI), lat/Math.PI));
             sphereverts.push(vec4(Math.sin(lat+step)*Math.cos(lon+step), Math.sin(lon+step)*Math.sin(lat+step), Math.cos(lat+step), 1.0)); //etc
             sphereverts.push(vec4(Math.sin(lat+step)*Math.cos(lon+step), Math.sin(lon+step)*Math.sin(lat+step), Math.cos(lat+step), 0.0));
-            sphereverts.push(vec2((lon+step)/(2*Math.PI), (lat+step)/Math.PI));
 
             //triangle 2
             sphereverts.push(vec4(Math.sin(lat+step)*Math.cos(lon+step), Math.sin(lon+step)*Math.sin(lat+step), Math.cos(lat+step), 1.0));
             sphereverts.push(vec4(Math.sin(lat+step)*Math.cos(lon+step), Math.sin(lon+step)*Math.sin(lat+step), Math.cos(lat+step), 0.0));
-            sphereverts.push(vec2((lon+step)/(2*Math.PI), (lat+step)/Math.PI));
             sphereverts.push(vec4(Math.sin(lat+step)*Math.cos(lon), Math.sin(lat+step)*Math.sin(lon), Math.cos(lat+step), 1.0));
             sphereverts.push(vec4(Math.sin(lat+step)*Math.cos(lon), Math.sin(lat+step)*Math.sin(lon), Math.cos(lat+step),0.0));
-            sphereverts.push(vec2(lon/(2*Math.PI), (lat+step)/Math.PI));
             sphereverts.push(vec4(Math.sin(lat)*Math.cos(lon), Math.sin(lon)*Math.sin(lat), Math.cos(lat), 1.0));
             sphereverts.push(vec4(Math.sin(lat)*Math.cos(lon), Math.sin(lon)*Math.sin(lat), Math.cos(lat), 0.0));
-            sphereverts.push(vec2(lon/(2*Math.PI), lat/Math.PI));
         }
     }
 
@@ -242,15 +215,6 @@ function mouse_up(){
 
 window.addEventListener("keydown" ,function(event){
     switch(event.key) {
-        case "r":
-            if(rotate){
-                rotate = false;
-                yAngle = earthRot;
-                earthRot = 0;
-            }else{
-                rotate = true;
-            }
-            break;
         case "g":
             switchShaders(GOURAUD);
             break;
@@ -263,62 +227,28 @@ window.addEventListener("keydown" ,function(event){
         case "c": //random color
             switchShaders(CEL);
             break;
+        default:
+            switchShaders(UNLIT);
     }
     requestAnimationFrame(render);//and now we need a new frame since we made a change
 });
-
-function initTextures() {
-    earthtex = gl.createTexture();
-    earthimage = new Image();
-    earthimage.onload = function() { handleTextureLoaded(earthimage, earthtex); }
-    earthimage.src = 'Earth.png';
-}
-
-function handleTextureLoaded(image, texture) {
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-
-    gl.generateMipmap(gl.TEXTURE_2D);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR));//gl.NEAREST); //if you want to see some aliasing
-    anisotropic_ext = gl.getExtension('EXT_texture_filter_anisotropic');
-    gl.texParameterf(gl.TEXTURE_2D, anisotropic_ext.TEXTURE_MAX_ANISOTROPY_EXT, factor);
-    gl.bindTexture(gl.TEXTURE_2D, null); //we aren't bound to any textures now
-}
-
-function update(){
-    if(rotate){
-        earthRot += .5;
-        while (earthRot >= 360){
-            earthRot -= 360;
-        }
-    }
-    requestAnimationFrame(render);
-}
 
 //draw a frame
 function render(){
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     //position camera 10 units back from origin
-    mv = lookAt(vec3(0, 0, 5), vec3(0, 0, 0), vec3(0, 1, 0));
+    mv = lookAt(vec3(0, 0, 10), vec3(0, 0, 0), vec3(0, 1, 0));
 
     //rotate if the user has been dragging the mouse around
-
     mv = mult(mv, mult(rotateY(yAngle), rotateX(xAngle)));
-
 
     //send the modelview matrix over
     gl.uniformMatrix4fv(umv[activeProgram], false, flatten(mv));
 
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, earthtex);
-    gl.uniform1i(uTextureSampler, 0);
-
     //note that if we have one value that should be applied to all the vertices,
     //we can send it over just once even if it's an attribute and not a uniform
-    gl.vertexAttrib4fv(vAmbientDiffuseColor[activeProgram], vec4(0, 0, .2, 1));
+    gl.vertexAttrib4fv(vAmbientDiffuseColor[activeProgram], vec4(.5, 0, 0, 1));
 
     if(activeProgram != 0) { //if we actually have lights
         gl.vertexAttrib4fv(vSpecularColor[activeProgram], vec4(1.0, 1.0, 1.0, 1.0)); //setting specular highlight to full white instead of red. glossy highlights are the color of the light
@@ -328,13 +258,5 @@ function render(){
         gl.uniform4fv(light_color[activeProgram], vec4(1, 1, 1, 1)); //1 is as bright as you can make it
         gl.uniform4fv(ambient_light[activeProgram], vec4(.5, .5, .5, 1)); //every single spot is getting at least 50% light, which is pretty high (.1 more realistic)
     }
-
-    var earthMV = mult(mv, translate(0, 0, 0));
-
-    if(rotate){
-        earthMV = mult(earthMV, rotateZ(earthRot));
-    }
-
-    gl.uniformMatrix4fv(umv[activeProgram], false, flatten(earthMV));
-    gl.drawArrays(gl.TRIANGLES, 0, sphereverts.length/3);
+    gl.drawArrays(gl.TRIANGLES, 0, sphereverts.length/2);
 }
